d253e036e9caed2d768ecbf1e3fcfe50d9f0b416
Lee Christensen <christensen.lee@gmail.com>
2021-06-03 10:40:01 -0400

    Fixing drop down

--- src/app/draft/draft.component.ts
+++ src/app/draft/draft.component.ts
@@ -4,7 +4,7 @@ import { Drafts } from './models/drafts';
 import { DraftService } from './service/draft.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TeamsHelper } from '../shared/teams-helper';
 
 @Component({
@@ -16,7 +16,8 @@ export class DraftComponent implements OnInit {
   draft$!: Observable<Drafts>;
   round: Partial<Round> = {};
 
-  draftYear = new Date().getFullYear();
+  baseDraftYear = new Date().getFullYear();
+  selectedDraftYear = this.baseDraftYear;
 
   draftYears = new Array(60);
 
@@ -24,6 +25,7 @@ export class DraftComponent implements OnInit {
 
   constructor(private svcDraft: DraftService,
               private route: ActivatedRoute,
+              private router: Router,
               public teamsHlp: TeamsHelper) {
               }
 
@@ -31,7 +33,7 @@ export class DraftComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       if (params.has('year')){
         // tslint:disable-next-line: no-non-null-assertion
-        this.draftYear = parseInt(params.get('year')!, 10);
+        this.selectedDraftYear = parseInt(params.get('year')!, 10);
       }
       if ( params.has('round')){
         // tslint:disable-next-line: no-non-null-assertion
@@ -39,9 +41,9 @@ export class DraftComponent implements OnInit {
       }
 
       for (let index = 0; index < 60; index++) {
-        this.draftYears[index] = this.draftYear - index;
+        this.draftYears[index] = this.baseDraftYear - index;
       }
-      this.getYear(this.draftYear.toString());
+      this.getDraftForYear(this.selectedDraftYear.toString());
     });
   }
 
@@ -58,9 +60,10 @@ export class DraftComponent implements OnInit {
   }
 
   getYear(year: string): void{
-    const val = year === null ? this.draftYear : year;
+    const val = year === null ? this.selectedDraftYear : year;
     // tslint:disable-next-line: no-non-null-assertion
-    this.draftYear = parseInt(year, 10);
+    this.selectedDraftYear = parseInt(year, 10);
+    this.router.navigate(['/draft', year, 1]);
     this.getDraftForYear(val.toString());
   }
 
